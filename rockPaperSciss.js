bc2cb508ad2fddfa2b42803c3c8dc70a5333f6b0
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-02 22:13:10 +0100

    Update rockPaperSciss.js
    -Added ;

--- rockPaperSciss.js
+++ rockPaperSciss.js
@@ -1,10 +1,10 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id
