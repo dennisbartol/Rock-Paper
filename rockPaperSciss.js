d560133dde616ba4664d12657baeab8dc015f7e2
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-03 16:50:50 +0100

    Update rockPaperSciss.js

--- rockPaperSciss.js
+++ rockPaperSciss.js
@@ -14,7 +14,7 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 }))
 
 function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1 // or you can use possibleChoices.length
+  const randomNumber = Math.floor(Math.random() * 3) + 1 
   
   if (randomNumber === 1) {
     computerChoice = 'rock'
